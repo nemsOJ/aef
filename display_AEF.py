d67bd6676353be89363c5350ef03cd33648a3527
Camoht <104215342+Camoht@users.noreply.github.com>
2023-12-06 15:48:24 +0100

    change names of var

--- display_AEF.py
+++ display_AEF.py
@@ -1,32 +1,33 @@
-all_matrice = [['#verouiller', '#deverouiller'], ['pousser', 'jeton'], [[[], [1, 0]], [[0], [1]]]]
-
-def display(all_matrice) :
+def displayAef(aef) :
     # Display the AEF in the terminal.
     # Takes a 3 dimensionnal list.
 
     print('Voici les noeuds de l\'AEF :')
-    for i in all_matrice[0] :
+    for i in aef[0] :
         print(i)
     print()
 
     print('Voici les symboles de l\'AEF :')
-    for j in all_matrice[1] :
+    for j in aef[1] :
         print(j)
     print()
 
     print('Voici liens de l\'AEF:')
-    for i in range(len(all_matrice[0])) :
-        for j in range(len(all_matrice[1])) :
-            for k in range(len(all_matrice[2][i][j])) :
-                print(all_matrice[0][i], '->', all_matrice[1][j], '->', all_matrice[0][int(all_matrice[2][i][j][k])])
+    for i in range(len(aef[0])) :
+        for j in range(len(aef[1])) :
+            for k in range(len(aef[2][i][j])) :
+                print(aef[0][i], '->', aef[1][j], '->', aef[0][int(aef[2][i][j][k])])
 
-def display_int(all_matrice) :
+def displayAefWithIndex(aef) :
     # Display the AEF in the terminal with the index.
     # Takes a 3 dimensionnal list.
     
-    for i in range(len(all_matrice[2])) :
-        for j in range(len(all_matrice[2][i])) :
-            for k in range(len(all_matrice[2][i][j])) :
-                print(i, '->', j, '->', all_matrice[2][i][j][k])
+    for i in range(len(aef[2])) :
+        for j in range(len(aef[2][i])) :
+            for k in range(len(aef[2][i][j])) :
+                print(i, '->', j, '->', aef[2][i][j][k])
 
-# display(all_matrice)
+'''Test
+aef = [['#verouiller', '#deverouiller'], ['pousser', 'jeton'], [[[], [1, 0]], [[0], [1]]]]
+displayAef(aef)
+'''
